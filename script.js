5731d0af23a55402d997c261c8d62ddf746d7af4
Kamilly <pkamillymattana@gmail.com>
2026-01-14 19:46:56 -0300

    Add ação de selecionar um item, alerta de item escolhido

---
+++ script.js
@@ -0,0 +1,6 @@
+let btnBurguer = document.getElementsByClassName("botaoBurguer")[0];
+let msg = document.getElementsByClassName("mensagem")[0];
+
+btnBurguer.addEventListener("click", function (){
+    alert("Hamburguer escolhido!");
+});
